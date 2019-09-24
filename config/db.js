2f77532bc22f2509af817b624b62d6e7f745eb25
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-24 15:58:43 +0200

    Changed Stuff

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
 	try {
 		await mongoose.connect(db, {
 			useNewUrlParser: true,
-			useUnifiedTopology: true
+			useUnifiedTopology: true,
+			useCreateIndex: true
 		})
 		console.log('successful connection to mongoose database')
 	} catch {
