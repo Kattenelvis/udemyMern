7f05fefc2eb8e6acab48dd45e3665e08fc95945b
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-28 19:31:54 +0200

    Push

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
 import React from 'react'
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/layoult/Navbar'
 import Landing from './components/layoult/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 function App() {
 	return (
 		<div className='App'>
-			<Navbar />
-			<Landing />
+			<Router>
+				<Navbar />
+				<Route exact path='/'>
+					<Landing />
+				</Route>
+				<section className='container'>
+					<Route exact path='/register'>
+						<Register />
+					</Route>
+					<Route exact path='/login'>
+						<Login />
+					</Route>
+				</section>
+			</Router>
 		</div>
 	)
 }
